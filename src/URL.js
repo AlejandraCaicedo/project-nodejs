a83fafedb6e8936209e01da2f6de59ba3a30cdcf
AlejandraCaicedo <caicedomaria905@gmail.com>
2022-03-09 18:15:52 -0500

    Development for the proposed mini project
    I made the solution using an async function and another function that returns a promise.

---
+++ src/URL.js
@@ -0,0 +1,4 @@
+const SHOW = "https://api.tvmaze.com/search/shows?q=";
+const EPISODES = "https://api.tvmaze.com/shows/";
+
+module.exports = { SHOW, EPISODES };
---
+++ src/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const { SHOW, EPISODES } = require("./URL");
+const axios = require("axios");
+
+async function getData(keyword) {
+  try {
+    const response = await axios.get(`${SHOW}${keyword}`);
+    const data = await response.data;
+
+    const firstData = data[0];
+    const firstShow = firstData.show;
+
+    let episodes = await getEpisodes(firstShow.id);
+
+    for (const episode of episodes) {
+      episode.idShow = firstShow.id;
+    }
+
+    console.log(episodes);
+    
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+function getEpisodes(id) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${EPISODES}${id}/episodes`)
+      .then(function (response) {
+        resolve(response.data);
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+}
+
+getData("vikings");
